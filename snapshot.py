d1910d62256271dae125cd8b68d3043d7e7ac5a2
m0rganch3n <morganchen@utexas.edu>
2017-09-03 17:50:44 -0500

    Code for Camera Stick
    Tested.

--- snapshot.py
+++ snapshot.py
@@ -1,2 +1,20 @@
 # Morgan, paste your code here.
-Change
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
+
+buttonPin = 7
+
+buttonPress = True
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+camera = PiCamera()
+
+while True:
+    buttonPress = GPIO.input(buttonPin)
+    if buttonPress == False:
+        camera.capture("/home/pi/Desktop/picture.jpg")
+
